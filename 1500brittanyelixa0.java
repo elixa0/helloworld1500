8e658bced59da3c057da5f9c7b13aee47ed3305b
elixa0 <77462302+elixa0@users.noreply.github.com>
2021-01-20 13:56:42 -0500

    Add files via upload

---
+++ 1500brittanyelixa0.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cis1500.winter2021.elixa0;
+
+/**
+ * Print Hello World!
+ * @author brittany mielke
+ */
+public class CIS1500Winter2021Elixa0 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.println("Hello, World");
+      System.out.println("Hello Computer, this is brittany");
+      System.out.println("Hi brittany, how are you today?");
+      System.out.println("Im having a bad day computer, I typed my password in where everyone could see it!");
+        
+ }
+    
+}
